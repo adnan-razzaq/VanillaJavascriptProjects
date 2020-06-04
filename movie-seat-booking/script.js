efc322f198674cb086333c158b013c05de1dd834
Adnan Razzaq <adnanrazzaq451@gmail.com>
2020-06-05 02:53:09 +0500

    storing data in localstorage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -2,27 +2,53 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied");
 const allseats = document.querySelectorAll(".row .seat");
 const screen = document.querySelector(".screen");
-const count = document.getElementById("count");
-const total = document.querySelector(".total");
-const movieSelect = document.getElementById("movie");
+let count = document.getElementById("count");
+let total = document.querySelector(".total");
+let movieSelect = document.getElementById("movie");
 // converting to type int
 let ticketPrice = +movieSelect.value;
 
+window.addEventListener("DOMContentLoaded", () => {
+  const selectedseats = JSON.parse(localStorage.getItem("selectedseats"));
+  console.log(selectedseats);
+
+  const Price = localStorage.getItem("Price");
+  const MovieIndex = localStorage.getItem("selectedMovieindex");
+  if (selectedseats !== null) {
+    seats.forEach((seat, index) => {
+      if (selectedseats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+    count.innerText = selectedseats.length;
+  }
+  if (MovieIndex !== null) {
+    movieSelect.selectedIndex = MovieIndex;
+  }
+  if (Price !== null) {
+    total.innerText = Price * selectedseats.length;
+  }
+});
 /* Functions */
 //update selected number of seats
 const updateSelectedCount = () => {
   const selectedseats = document.querySelectorAll(".row .seat.selected");
   //getting index of selected seats
   const seatindex = [...selectedseats].map((seat) => {
-    //console.log(allseats);
     const index = [...allseats].indexOf(seat);
     return index;
   });
-  console.log(seatindex);
-
+  // seeting up localstorage
+  localStorage.setItem("selectedseats", JSON.stringify(seatindex));
   const selectedLength = selectedseats.length;
   count.innerText = selectedLength;
   total.innerText = selectedLength * ticketPrice;
+};
+
+//function foer localStorage
+const setMoviedata = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieindex", movieIndex);
+  localStorage.setItem("Price", moviePrice);
 };
 
 //eventlistener for selecting seats
@@ -45,6 +71,10 @@ container.addEventListener("click", (e) => {
 //eventListener for select element
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+
+  //adding data to localStorage
+  setMoviedata(e.target.selectedIndex, e.target.value);
+
   //removing selected seat on changing movie
   seats.forEach((seat) => {
     seat.classList.remove("selected");
